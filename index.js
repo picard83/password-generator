c92320565fa23df4411e6488698f456d102ac9f3
picard83 <picard83@hotmail.com>
2023-12-30 21:32:03 -0500

    prompts finished

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ const password = document.querySelector(".password");
 const container = document.querySelector(".container");
 
 btn.addEventListener("click", function () {
-  collection();
+  collectionOfPrompts();
 });
 
-function collection() {
+function collectionOfPrompts() {
   let name = prompt("whats your name?");
   let age = prompt("how old are you?");
   let location = prompt("whats your favorite vacation spot (one word) ?");
-
-  // let length = prompt("how long do you want your password? max 8 ");
   let collection = [name + age + location];
   // console.log(collection);
-  password.append(collection);
+  password.append(`Your New Password: ${collection}`);
 }
